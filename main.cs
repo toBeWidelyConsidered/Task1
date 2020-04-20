02db0d0dcc937832750b4258cb7d113cbc320ffb
toBeWidelyConsidered <hitrova.zoya1533@gmail.com>
2020-04-20 18:43:40 +0300

    create main.cs

---
+++ main.cs
@@ -0,0 +1,11 @@
+using System;
+
+class MainClass {
+  static void Main (string[] args) {
+        Console.WriteLine("Hello World");
+        int a = 4, b = 3;
+        int c = a + b;
+        Console.WriteLine(c);
+        Console.ReadKey();
+  }
+}
