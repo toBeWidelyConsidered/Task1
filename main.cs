d94897001928e4370fea5d4960af45b8ac1d4fec
v1sk3o <47939237+v1sk3o@users.noreply.github.com>
2020-04-24 18:27:34 +0300

    Update main.cs

--- main.cs
+++ main.cs
@@ -1,11 +1,18 @@
 using System;
-
-class MainClass {
-  static void Main (string[] args) {
-        Console.WriteLine("Hello World");
-        int a = 4, b = 3;
-        int c = a + b;
-        Console.WriteLine(c);
-        Console.ReadKey();
-  }
+public class FibonacciExample
+{
+    public static void Main(string[] args)
+    {
+        int n1 = 0, n2 = 1, n3, i, number;
+        Console.Write("Enter the number of elements: ");
+        number = int.Parse(Console.ReadLine());
+        Console.Write(n1 + " " + n2 + " ");
+        for (i = 2; i < number; ++i)
+        {
+            n3 = n1 + n2;
+            Console.Write(n3 + " ");
+            n1 = n2;
+            n2 = n3;
+        }
+    }
 }
